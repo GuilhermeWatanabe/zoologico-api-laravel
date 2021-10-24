76880c8c00b63b78c5e461935e09a7c8c1e0617d
Guilherme Watanabe <watanab430@gmail.com>
2021-10-24 19:27:12 -0300

    Feat: create the model, controller and migration for the animals.

---
+++ app/Models/Animal.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Animal extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'nickname',
+        'scientific_name',
+        'password',
+        'zoo_wing',
+        'image_url',
+    ];
+}
---
+++ database/migrations/2021_10_24_222136_create_animals_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnimalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->id();
+            $table->string('nickname');
+            $table->string('scientific_name');
+            $table->string('password');
+            $table->string('zoo_wing');
+            $table->string('image_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('animals');
+    }
+}
