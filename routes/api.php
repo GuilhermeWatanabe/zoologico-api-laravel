d0e2b3d3d47fd8de33ad22f3dab7de4985457bcd
Guilherme Watanabe <watanab430@gmail.com>
2021-11-01 20:45:35 -0300

    Fix: add the routes to the authentication group.

--- routes/api.php
+++ routes/api.php
@@ -17,22 +17,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//auth routes
 Route::post('login', [AuthController::class, 'login']);
 
-//animals routes
-Route::get('animal', [AnimalController::class, 'index']);
+Route::post('janitor', [JanitorController::class, 'store']);
+
 Route::post('animal', [AnimalController::class, 'store']);
-//used POST to the update method because i can't upload files with PUT/PATH
-Route::post('animal/{id}', [AnimalController::class, 'update']);
-Route::patch('animal/{id}', [AnimalController::class, 'disable']);
 
 Route::group(['middleware' => ['apiJWT']], function () {
     Route::post('logout', [AuthController::class, 'logout']);
-
+    
+    //animals routes
+    Route::get('animal', [AnimalController::class, 'index']);
+    //used POST to the update method because i can't upload files with PUT/PATH
+    Route::post('animal/{id}', [AnimalController::class, 'update']);
+    Route::patch('animal/{id}', [AnimalController::class, 'disable']);
     Route::patch('animal/voting/{id}', [AnimalController::class, 'voting']);
     Route::get('animal/to-vote', [AnimalController::class, 'toVote']);
 });
 
-//janitors routes
-Route::post('janitor', [JanitorController::class, 'store']);
